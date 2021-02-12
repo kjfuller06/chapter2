d6bedb0195d1f29bdfd4c0f56c81a0a044d6711d
Kathryn Fuller <90946112@westernsydney.edu.au>
2021-02-12 16:47:44 -0600

    Trying a much smaller sample of data

--- scripts/gedi_download.R
+++ scripts/gedi_download.R
@@ -9,13 +9,13 @@ library(leaflet)
 library(leafsync)
 
 # Study area boundary box coordinates
-xmax = 150.73487
-ymax = -33.61501
-xmin = 150.74169
-ymin = -33.62048
+xmax = 150.7415
+ymax = -33.6155
+xmin = 150.741
+ymin = -33.616
 
 # Specifying the date range
-daterange=c("2019-01-01","2021-01-01")
+daterange=c("2019-01-01","2019-10-01")
 
 # Get path to GEDI data
 gLevel1B<-gedifinder(product="GEDI01_B", xmax, ymax, xmin, ymin, version="001",daterange=daterange)
@@ -23,16 +23,16 @@ gLevel2A<-gedifinder(product="GEDI02_A", xmax, ymax, xmin, ymin, version="001",d
 gLevel2B<-gedifinder(product="GEDI02_B", xmax, ymax, xmin, ymin, version="001",daterange=daterange)
 
 # Set output dir for downloading the files
-outdir="data/gedi/richmond"
+outdir="data/gedi/richmond/tiny"
 
 # Downloading GEDI data
-# gediDownload(filepath=gLevel1B,outdir=outdir) # geolocated waveforms
-# gediDownload(filepath=gLevel2A,outdir=outdir) # elevation and height metrics
+gediDownload(filepath=gLevel1B,outdir=outdir) # geolocated waveforms
+gediDownload(filepath=gLevel2A,outdir=outdir) # elevation and height metrics
 gediDownload(filepath=gLevel2B,outdir=outdir) # canopy cover and vertical profile metrics
 
 # Reading GEDI data
 files <- list.files(outdir, full.names = TRUE)
-gedilevel1b<-readLevel1B(paste0(files[1]))
+gedilevel1b<-readLevel1B(paste0(outdir, "\\GEDI01_B_2019253111453_O04217_T02086_02_003_01.h5"))
 
 # get GEDI puls geolocation
 level1bGeo<-getLevel1BGeo(level1b=gedilevel1b,select=c("elevation_bin0"))
@@ -45,7 +45,8 @@ level1bGeo$shot_number<-paste0(level1bGeo$shot_number)
 level1bGeo_spdf<-SpatialPointsDataFrame(cbind(level1bGeo$longitude_bin0, level1bGeo$latitude_bin0), data=level1bGeo)
 
 # Exporting level1bGeo as ESRI Shapefile
-raster::shapefile(level1bGeo_spdf, substr(files[1], 1, nchar(files[1])-3))
+raster::shapefile(level1bGeo_spdf, paste0(outdir, "\\GEDI01_B_2019253111453_O04217_T02086_02_003_01_sub"))
+
 # level1bGeo_spdf = st_read(paste0(substr(files[1], 1, nchar(files[1])-3), ".shp"))
 
 # mapping gedi data
@@ -79,8 +80,9 @@ head(level2AM[,c("beam","shot_number","elev_highestreturn","elev_lowestmode","rh
 # Converting shot_number as "integer64" to "character"
 level2AM$shot_number<-paste0(level2AM$shot_number)
 
-# selecting only shot numbers with positive vegetation height data
-level2AM = level2AM[level2AM$rh90 > 0,]
+# selecting only GEDI data with valid vegetation height data
+level2AM = level2AM %>% 
+  dplyr::filter(rh0 >= -213 & rh0 <= 213 & rh100 >= -213 & rh100 <= 213)
 
 # Converting Elevation and Height Metrics as data.table to SpatialPointsDataFrame
 level2AM_spdf<-SpatialPointsDataFrame(cbind(level2AM$lon_lowestmode,level2AM$lat_lowestmode),
